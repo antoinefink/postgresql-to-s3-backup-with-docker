84efcbd56a391b991a3803605ee70c7f657e4d04
Antoine Finkelstein <antoinefinkelstein@users.noreply.github.com>
2016-11-03 10:43:27 +0100

    Add server side encryption to all uploads

--- startup.sh
+++ startup.sh
@@ -77,6 +77,6 @@ website_error =
 website_index = index.html
 EOL
 # Dumping the database and upload the database
-pg_dump -h $DATABASE_IP -p $DATABASE_PORT -U $DATABASE_USERNAME -Fc $DATABASE_NAME | s3cmd put - s3://$DESTINATION/$newname
+pg_dump -h $DATABASE_IP -p $DATABASE_PORT -U $DATABASE_USERNAME -Fc $DATABASE_NAME | s3cmd --server-side-encryption put - s3://$DESTINATION/$newname
 
 echo "$DATABASE_NAME backup successful"
